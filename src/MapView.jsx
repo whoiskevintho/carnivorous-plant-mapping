b7c3012d2cf7d9c6c8fb093c9262eb007c50f923
Kevin Young <youngkevinnc@gmail.com>
2025-10-16 13:49:23 -0400

    Initial commit: Maplibre React setup

---
+++ src/MapView.jsx
@@ -0,0 +1,30 @@
+import { useEffect, useRef } from "react";
+import maplibregl from "maplibre-gl";
+
+export default function MapView() {
+  const mapContainer = useRef(null);
+  const map = useRef(null);
+
+  useEffect(() => {
+    if (map.current) return; // initialize map only once
+
+    map.current = new maplibregl.Map({
+      container: mapContainer.current,
+      style: "https://api.maptiler.com/maps/streets-v2/style.json?key=KDJVRGphbKUQxUNhl8qL",
+      center: [-74.5, 40], // longitude, latitude
+      zoom: 9,
+    });
+
+    new maplibregl.Marker().setLngLat([-74.5, 40]).addTo(map.current);
+  }, []);
+
+  return (
+    <div
+      ref={mapContainer}
+      style={{
+        width: "100%",
+        height: "100vh",
+      }}
+    />
+  );
+}
